e6d4a11695d98afb9e1ffd50fb4de7e96d3a6907
Saloni <salonimehta2127@gmail.com>
2021-05-11 20:49:21 -0400

    commit tree d3d5eb922a4de467f82aa8c7028f8abad28201e7

--- index.js
+++ index.js
@@ -5,31 +5,70 @@ function moveDodgerLeft()
 const leftNumbers=dodger.style.left.replace("px","");
 const left=parseInt(leftNumbers,10);
 if(left>0)
-{
-    dodger.style.left=`${left-1}px`;
+    { dodger.style.left= `${left-1}px`;
 }
 }
-
 function moveDodgerRight()
 {
-const rightNumbers=dodger.style.left.replace("px","");
-const left=parseInt(rightNumbers,10);
-
+const leftNumbers=dodger.style.left.replace("px","");
+const left=parseInt(leftNumbers,10);
 if(left<360)
 {
     dodger.style.left=`${left+1}px`;
 }
 }
-
-document.addEventListener("keydown",function(e)
-{
-    if(e.key==="ArrowLeft")
+document.addEventListener("keydown",(e)=>{
+    if(e.key==='ArrowLeft')
     {
         moveDodgerLeft();
     }
-    else if(e.key==="ArrowRight")
+    else if(e.key==='ArrowRight')
     {
         moveDodgerRight();
     }
-});
+})
 
+
+
+
+
+
+
+
+
+
+//prework attempt
+// const dodger=document.getElementById("dodger");
+
+// function moveDodgerLeft()
+// {
+// const leftNumbers=dodger.style.left.replace("px","");
+// const left=parseInt(leftNumbers,10);
+// if(left>0)
+// {
+//     dodger.style.left=`${left-1}px`;
+// }
+// }
+
+// function moveDodgerRight()
+// {
+// const rightNumbers=dodger.style.left.replace("px","");
+// const left=parseInt(rightNumbers,10);
+
+// if(left<360)
+// {
+//     dodger.style.left=`${left+1}px`;
+// }
+// }
+
+// document.addEventListener("keydown",function(e)
+// {
+//     if(e.key==="ArrowLeft")
+//     {
+//         moveDodgerLeft();
+//     }
+//     else if(e.key==="ArrowRight")
+//     {
+//         moveDodgerRight();
+//     }
+//      });
